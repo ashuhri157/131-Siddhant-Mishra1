93add50e4219c6fd77f1f720cfec23486ed26f6d
Siddhant Mishra <90673264+ashuhri157@users.noreply.github.com>
2022-01-10 17:44:09 +0530

    React

---
+++ Shopping-cart/src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Ohrensessel Josslyn',
+    price: 2500,
+    currency: 'EUR',
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'Sessel Sofa',
+    price: 5000,
+    currency: 'EUR',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 4,
+    name: 'Rovigo Sofa',
+    price: 2390,
+    currency: 'EUR',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 6,
+    name: 'Sessel Little Sofa',
+    price: 1190.99,
+    currency: 'EUR',
+    image: 'images/06.jpg',
+  },
+  {
+    id: 5,
+    name: 'Sessel Peacock Sofa',
+    price: 599.99,
+    currency: 'EUR',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'Sessel Sofa',
+    price: 14999,
+    currency: 'EUR',
+    image: 'images/03.jpg',
+  }
+];
+
+export default data;
